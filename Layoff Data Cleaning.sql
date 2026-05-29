50823c02015b50f5d8e6846e243d1de787a9c7d8
Sulocini T <sulocini887@gmail.com>
2026-05-29 16:49:27 +0530

    Update Layoff Data Cleaning.sql
    Create a staging table for processing layoff data to ensure raw data is preserved.

--- Layoff Data Cleaning.sql
+++ Layoff Data Cleaning.sql
@@ -5,7 +5,8 @@ FROM world_layoffs.layoffs;
 
 
 
--- first thing we want to do is create a staging table. This is the one we will work in and clean the data. We want a table with the raw data in case something happens
+-- creating staging table--
+
 CREATE TABLE world_layoffs.layoffs_staging 
 LIKE world_layoffs.layoffs;
 
