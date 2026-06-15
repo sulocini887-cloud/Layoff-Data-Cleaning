e818760e8dcc86f129005589bf233e3ad983a201
Sulocini T <sulocini887@gmail.com>
2026-06-15 15:57:02 +0530

    Clean and standardize layoff data in SQL script
    Refactor SQL script to clean layoff data by removing duplicates, updating null values, standardizing industry names, and fixing date formats.

--- Layoff Data Cleaning.sql
+++ Layoff Data Cleaning.sql
@@ -23,8 +23,6 @@ SELECT * FROM world_layoffs.layoffs;
 
 -- 1. Remove Duplicates
 
-
-
 SELECT *
 FROM world_layoffs.layoffs_staging
 ;
@@ -37,54 +35,44 @@ FROM world_layoffs.layoffs_staging;
 
 
 SELECT *
-FROM (
-	SELECT company, industry, total_laid_off,`date`,
+FROM (SELECT company, industry, total_laid_off,`date`,
 		ROW_NUMBER() OVER (
-			PARTITION BY company, industry, total_laid_off,`date`
-			) AS row_num
-	FROM 
-		world_layoffs.layoffs_staging
-) duplicates
+		PARTITION BY company, industry, total_laid_off,`date`) 
+		AS row_num
+		FROM 
+		world_layoffs.layoffs_staging) duplicates
 WHERE 
 	row_num > 1;
 
-    
--- let's just look at oda to confirm
 SELECT *
 FROM world_layoffs.layoffs_staging
 WHERE company = 'Oda'
 ;
--- it looks like these are all legitimate entries and shouldn't be deleted. We need to really look at every single row to be accurate
 
--- these are our real duplicates 
 SELECT *
 FROM (
 	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
 		ROW_NUMBER() OVER (
-			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
-			) AS row_num
+		PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions)
+		AS row_num
 	FROM 
-		world_layoffs.layoffs_staging
-) duplicates
+		world_layoffs.layoffs_staging)
+		duplicates
 WHERE 
 	row_num > 1;
 
--- these are the ones we want to delete where the row number is > 1 or 2or greater essentially
-
-
--- now you may want to write it like this:
+-- these are the ones we want to delete where the row number is > 1 or 2
 
 WITH DELETE_CTE AS 
 (
 SELECT *
 FROM (
 	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
-		ROW_NUMBER() OVER (
-			PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions
-			) AS row_num
+		ROW_NUMBER() OVER (PARTITION BY company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions)
+		AS row_num
 	FROM 
-		world_layoffs.layoffs_staging
-) duplicates
+		world_layoffs.layoffs_staging)
+		duplicates
 WHERE 
 	row_num > 1
 )
@@ -104,8 +92,7 @@ WHERE (company, location, industry, total_laid_off, percentage_laid_off, `date`,
 	FROM DELETE_CTE
 ) AND row_num > 1;
 
--- one solution, which I think is a good one. Is to create a new column and add those row numbers in. Then delete where row numbers are over 2, then delete that column
--- so let's do it!!
+--  create a new column and add those row numbers in. Then delete where row numbers are over 2, then delete that column
 
 ALTER TABLE world_layoffs.layoffs_staging ADD row_num INT;
 
@@ -164,7 +151,6 @@ WHERE row_num >= 2;
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
 
--- if we look at industry it looks like we have some null and empty rows, let's take a look at these
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -175,7 +161,7 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- let's take a look at these
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE company LIKE 'Bally%';
@@ -184,12 +170,7 @@ SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE company LIKE 'airbnb%';
 
--- it looks like airbnb is a travel, but this one just isn't populated.
--- I'm sure it's the same for the others. What we can do is
--- write a query that if there is another row with the same company name, it will update it to the non-null industry values
--- makes it easy so if there were thousands we wouldn't have to manually check them all
-
--- we should set the blanks to nulls since those are typically easier to work with
+---
 UPDATE world_layoffs.layoffs_staging2
 SET industry = NULL
 WHERE industry = '';
@@ -202,7 +183,7 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- now we need to populate those nulls if possible
+----
 
 UPDATE layoffs_staging2 t1
 JOIN layoffs_staging2 t2
@@ -211,7 +192,7 @@ SET t1.industry = t2.industry
 WHERE t1.industry IS NULL
 AND t2.industry IS NOT NULL;
 
--- and if we check it looks like Bally's was the only one without a populated row to populate this null values
+----
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE industry IS NULL 
@@ -220,7 +201,7 @@ ORDER BY industry;
 
 -- ---------------------------------------------------
 
--- I also noticed the Crypto has multiple different variations. We need to standardize that - let's say all to Crypto
+--Crypto has multiple different variations. 
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
@@ -229,18 +210,18 @@ UPDATE layoffs_staging2
 SET industry = 'Crypto'
 WHERE industry IN ('Crypto Currency', 'CryptoCurrency');
 
--- now that's taken care of:
+----
 SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
 
 -- --------------------------------------------------
--- we also need to look at 
+
 
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- everything looks good except apparently we have some "United States" and some "United States." with a period at the end. Let's standardize this.
+---
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
@@ -248,21 +229,21 @@ ORDER BY country;
 UPDATE layoffs_staging2
 SET country = TRIM(TRAILING '.' FROM country);
 
--- now if we run this again it is fixed
+-- 
 SELECT DISTINCT country
 FROM world_layoffs.layoffs_staging2
 ORDER BY country;
 
 
--- Let's also fix the date columns:
+--  fixing the date columns:
 SELECT *
 FROM world_layoffs.layoffs_staging2;
 
--- we can use str to date to update this field
+-- using str to date to update this field
 UPDATE layoffs_staging2
 SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
 
--- now we can convert the data type properly
+-- converting the data type properly
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;
 
