335528b9f75ae8b0cebb4b75056b47678b7311b7
Sulocini T <sulocini887@gmail.com>
2026-06-16 11:29:41 +0530

    Update EDA (Exploratory Data Analysis.sql

--- EDA (Exploratory Data Analysis.sql
+++ EDA (Exploratory Data Analysis.sql
@@ -6,7 +6,6 @@
 SELECT * 
 FROM world_layoffs.layoffs_staging2;
 
--- EASIER QUERIES
 
 SELECT MAX(total_laid_off)
 FROM world_layoffs.layoffs_staging2;
@@ -20,6 +19,7 @@ FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off IS NOT NULL;
 
 -- Which companies had 1 which is basically 100 percent of they company laid off
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
@@ -27,6 +27,7 @@ WHERE  percentage_laid_off = 1;
 -- these are mostly startups it looks like who all went out of business during this time
 
 -- if we order by funcs_raised_millions we can see how big some of these companies were
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1
@@ -112,7 +113,8 @@ FROM layoffs_staging2
 GROUP BY dates
 ORDER BY dates ASC;
 
--- now use it in a CTE so we can query off of it
+-- using it in a CTE so we can query off of it
+
 WITH DATE_CTE AS 
 (
 SELECT SUBSTRING(date,1,7) as dates, SUM(total_laid_off) AS total_laid_off
